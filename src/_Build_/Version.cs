998900b96e277e816b0ff1a5ad2e16660a8075e6
twilker <twilker@phoenixcontact.com>
2021-05-11 07:21:07 +0200

    Fix readmes #exported-commit:3cf58d200ab1eb46c494894817695d699edc8b55

---
+++ src/_Build_/Version.cs
@@ -0,0 +1,14 @@
+﻿#region Copyright
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) Phoenix Contact GmbH & Co KG
+//  This software is licensed under Apache-2.0
+//
+///////////////////////////////////////////////////////////////////////////////
+#endregion
+
+internal static class Version
+{
+    // assembly version number
+    internal const string AssemblyVersion = "1.0.0.0";
+}
